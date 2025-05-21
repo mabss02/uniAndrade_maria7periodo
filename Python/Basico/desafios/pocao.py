ab0d170e32f9a9b98f8197c5ab5baf5aeadc17fa
mabss02 <149502065+mabss02@users.noreply.github.com>
2025-05-21 23:15:46 +0000

    S

--- Python/Basico/desafios/pocao.py
+++ Python/Basico/desafios/pocao.py
@@ -37,11 +37,41 @@ p1.usar_pocaoRoxa(pocaoRoxa)
 #del p1
 print(pocao1)
 
-# Se o personagem ainda está vivo, decremente ao usar poção veneno
-    # Pode usar poção veneno
-    # Pode usar poção saude
-   
-# Se a saude for <= 0  
-    # personagem vivo=False
-    # informe personagem está morto, foi de "arrasta"
-    # cancele a possibilidade de incrementar ou decrementar saude
+class Player
+    def __init__(self, nome):
+        self.nome = nome
+        self.energia = 100
+        self.vivo = True
+
+    def usar_pocao(self, pocao):
+        if not self.vivo:
+            print("Game Over!")
+            return
+        
+        efeito_pocao = pocao.usar() # Número + ou -
+        nova_energia = efeito_pocao + self.energia
+
+        # Tratar casos (0 e 100+)
+        if nova_energia <=0;
+            nova_energia = 0
+            self.vivo = False
+
+        if nova_energia >= 100;
+            nova_energia = 100
+            print("Impossivel usar cura!")
+
+        self.energia = nova_energia
+        self.status()
+
+class Item:
+    def __init__(self, efeito):
+        self.tipo = tipo
+        self.efeito = efeito
+
+class Inventario:
+    def __init__(self):
+        self.itens = []
+
+    def adicionar_item (self, item: Item):
+        if not item:
+            print("Escolha um item para adicionar ao inventario")
