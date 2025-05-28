f4d87e9db4772b74c28c139924d7fbba868a4930
mabss02 <149502065+mabss02@users.noreply.github.com>
2025-05-28 23:06:32 +0000

    salve

---
+++ Python/Basico/modulos/main.py
@@ -0,0 +1,15 @@
+import player
+#from player import atacar, dados
+from player import * #importe dados
+
+def main():
+#atacar()
+#dados()
+    player.atacar()
+    player.dados()
+
+if __name__ == "__main__":
+    main()
+
+#print("Eu sou o main")
+#print(__name__)
---
+++ Python/Basico/modulos/player.py
@@ -0,0 +1,8 @@
+def dados():
+    print("Olá, sou o player")
+
+def atacar():
+    print("Fight")
+
+if __name__ == "__main__":
+    print("Eu sou o player")
