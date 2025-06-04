91eff5635ee2d2678476bbd537a7119bc7579624
mabss02 <149502065+mabss02@users.noreply.github.com>
2025-06-04 23:39:41 +0000

    S

--- Python/Basico/darkSouls/jogador.py
+++ Python/Basico/darkSouls/jogador.py
@@ -1,6 +1,8 @@
-class Jogador:
+from abc import ABC, abstractmethod
 
-    def __init__(self, nome, dano):
+class Jogador(ABC): #Herança
+
+    def __init__(self, nome:str, dano:int):
         self.nome = nome
         self.dano = dano
         self.__saude = 100 #encapsulamento
@@ -11,14 +13,16 @@ class Jogador:
     
     #@saude.setter # Decorador retorna apenas como propriedade
     def set_saude(self, valor):
-        self.saude += valor
+        self.saude += max(0,valor)
 
+    @abstractmethod # Obriga as classes filhas a implementarem
     def atacar(self):
         print(f"{self.nome} atacou!")
 
+    @abstractmethod # Obriga as classes filhas a implementarem
     def defender(self):
         print(f"{self.nome} defendeu!")
 
 if __name__ == '__main__':
-    p1 = jogador("Jow", 50)
+    p1 = Jogador("Jow", 50)
     p1.atacar()
